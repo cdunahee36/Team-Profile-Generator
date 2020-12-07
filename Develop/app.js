762082135f212dd00a67e92c3c724ba0817b47dc
cdunahee36 <dunahee10@gmail.com>
2020-12-06 22:56:43 -0600

    Set up the first prompt.

--- Develop/app.js
+++ Develop/app.js
@@ -4,11 +4,44 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./output/htmlRenderer");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+const Employees = [];
+  
+function startPrompt() {
+inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Please input member name.',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'Please input member role.',
+      name: 'role',
+    },
+    {
+      type: 'input',
+      message: 'Please input member email.',
+      name: 'Email',
+    },
+    {
+      type: 'input',
+      message: 'Please input member id.',
+      name: 'id',
+    },
+  ]);
+};
+
+startPrompt();
+
+
+
+
+
 
 
 // Write code to use inquirer to gather information about the development team members,
